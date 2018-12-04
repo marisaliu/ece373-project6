2b3844185e4718210cec9beaf1c722d7e1e5168b
ece373 <myliu@umass.edu>
2018-12-04 17:09:05 -0500

    started server

--- server.c
+++ server.c
@@ -3,25 +3,61 @@
  */
 /* $begin echoservertmain */
 #include "csapp.h"
-
+#include <string.h>
+#include <stdio.h>
+int maxClients = 0;
+int clientIndex = 0;
 void *thread(void *vargp);
 
 void echo(int connfd)
 {
-    int n;
-    char buf[MAXLINE];
-    rio_t rio;
+  int n;
+  char buf[MAXLINE];
+	char* toUser;
+  rio_t rio;
+	Rio_readlineb(&rio, buf, MAXLINE)
+	while(userArr[index] != NULL){
+		index++;
+	}
+	userArr[index] = buf; 
+	ipArr[index] = connfd;
+  Rio_readinitb(&rio, connfd);
+	while(1){
+		Rio_readlineb(&rio, buf, MAXLINE)
+		toUser = parseUser(buf)
+		if(strcmp(toUser, ERROR)){
+			strcpy(buf, "ERROR! NO USER OF THAT NAME!	Please enter a valid user");
+			rio_writen(connfd, buf, strlen(buf));
+		}
+		else{
+			int i = 0;
+			char *message = buf + strlen(toUser);
+			while(strcmp(userArr[i], toUser) != 0){
+				i = (i+1)%50;
+			}
+			if(strcmp(message, "quit") == 0) break;
+			if(strcmp(message, "list-users" == 0)){
+				for(int j = 0; j < 50; j++){
+					if(userArr[j] != NULL) rio_written(connfd, userArr[j], strlen(userArr[j]);
+				}
+			}
+			rio_writen(ipArr[i], message, strlen(message));
 
-    Rio_readinitb(&rio, connfd);
-    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-        printf("server received %d bytes\n", n);
-        Rio_writen(connfd, buf, n);
-    }
+		}
+
+
+	}
+//  while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+  //      printf("server received %d bytes\n", n);
+   //     Rio_writen(connfd, buf, n);
+    //}
 }
 
 int main(int argc, char **argv) 
 {
-    int listenfd, *connfdp;
+   char userArr[50][21];   //creates an array of 50 users with max username length of 20 character
+	 char ipArr[50][21];
+	 int listenfd, *connfdp;
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
     pthread_t tid; 
@@ -33,10 +69,15 @@ int main(int argc, char **argv)
     listenfd = Open_listenfd(argv[1]);
 
     while (1) {
-        clientlen=sizeof(struct sockaddr_storage);
-	connfdp = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
-	*connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
-	Pthread_create(&tid, NULL, thread, connfdp);
+      clientlen=sizeof(struct sockaddr_storage);
+			connfdp = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
+			*connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
+			if((numClients +1) >= maxClients){
+				printf("MAX CLIENTS REACHED \n");
+				close(connfd);
+			}
+
+			Pthread_create(&tid, NULL, thread, connfdp);
     }
 }
 
