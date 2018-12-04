dc2a33efa7b92438f4c6f68f83c5f0a69ee6fb3a
dylanb271 <dylanb271@gmail.com>
2018-12-04 17:12:00 -0500

    client

--- client.c
+++ client.c
@@ -9,22 +9,34 @@ int main(int argc, char **argv)
     int clientfd;
     char *host, *port, buf[MAXLINE];
     rio_t rio;
-
-    if (argc != 3) {
-	fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
-	exit(0);
+		
+    if (argc != 4) {
+			fprintf(stderr, "usage: %s <host> <port> <username>\n", argv[0]);
+			exit(0);
     }
+		if(strlen(argv[3]) > 20){
+			printf("username should not exceed 20 characters\n");
+			exit(0);
+		}
     host = argv[1];
     port = argv[2];
 
     clientfd = Open_clientfd(host, port);
     Rio_readinitb(&rio, clientfd);
-
-    while (Fgets(buf, MAXLINE, stdin) != NULL) {
-	Rio_writen(clientfd, buf, strlen(buf));
-	Rio_readlineb(&rio, buf, MAXLINE);
-	Fputs(buf, stdout);
+		//send username to server
+		Rio_writen(clientfd, argv[3], strlen(argv[3]));
+    //take input
+		while (1) {
+			printf("> ");
+			Fgets(buf, MAXLINE, stdin);
+			Rio_writen(clientfd, buf, strlen(buf));
+			Rio_readlineb(&rio, buf, MAXLINE);
+			if(strcmp(buf, "0")){
+				printf("error: no user with that name\n");
+			}
+			Fputs(buf, stdout);
     }
+		printf("GoodBye!\n");
     Close(clientfd); //line:netp:echoclient:close
     exit(0);
 }
